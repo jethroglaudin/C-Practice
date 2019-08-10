c5399e7ef13cc2ceca6478850f533f25f11d2da9
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-09 23:03:08 -0400

    working on dereferencing a pointer

---
+++ deRefPointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int age = 27;
+    int * pAge = &age;
+
+// derefencing will get the value whatever the adresss was storing
+
+    printf("%d\n", *pAge);
+    printf("%d\n", *&age);
+    return 0;
+}

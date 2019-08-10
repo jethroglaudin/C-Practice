415faf037df68d8889ddfd39299a7b655266de45
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-09 22:57:53 -0400

    working with arrays

---
+++ arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
+    // luckyNumbers[1] = 200;
+    int luckyNumbers[10];
+    luckyNumbers[1] = 80;
+    printf("%d\n", luckyNumbers[1]);
+    return 0;
+}

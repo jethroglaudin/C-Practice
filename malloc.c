a4c0e2acc69235e75176b0a77488739d76e29314
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-13 19:05:01 -0400

    updated malloc.c

--- malloc.c
+++ malloc.c
@@ -1,47 +1,24 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+int* return_me(int*);
+// returnType functionName (type arg1, type arg2)
+
 int main(void) 
 {
 
+int x = 10;
+// let p equal the memory address of x
+int *p = &x;
+printf("p: %p\n", p);
 
-// we can use pointers to get access to a block of dynamically-allocated memory 
-// at runtime
-// Dynamically allocated memory comes form a pool of memory known as 
-// the heap. 
-// All memory from a variable come from the stack.
+p = return_me(p);
+printf("p: %p\n", p);
 
-// statically create interger on the stack;
+return 0;
 
-
-
-int x;
-
-// dynaically obtain an interger
-int *px = malloc(4);
-// malloc will go find you 4 bytes of memory that are next to each
-// other on the heap and will return a pointer of that memory
-// called px.
-// int *px = malloc(sizeof(int));
-
-// get an interger from te user
-
-// int x = get_int();
-
-// // array of floats on the stack
-// float stack_array[x];
-
-// // array of floats on the heap
-// float* heap_array = malloc(x * sizeof(float));
-
-// // Malloc is not released back to the 
-
-
-// char * word = malloc(50 * sizeof(char));
-
-// // do stuff with word
-// // now we're don using it
-// free(word);
-
-
+}
+int* return_me(int *pint)
+{
+    return pint;
 }

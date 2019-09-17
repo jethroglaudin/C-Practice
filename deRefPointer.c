a305ce2caca7a5cc70b91b4790be70394ab0fb01
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-17 17:53:35 -0400

    updated

--- deRefPointer.c
+++ deRefPointer.c
@@ -6,9 +6,12 @@ int main ()
     int age = 27;
     int * pAge = &age;
 
+
+
+
 // derefencing will get the value whatever the adresss was storing
 
     printf("%d\n", *pAge);
-    printf("%d\n", *&age);
+    printf("%p\n", &age);
     return 0;
 }
